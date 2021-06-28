6bd00deaddf8895c54b1053dbf1789c8e94ef2a2
GusungLee <kckt66@naver.com>
2021-06-28 23:45:15 +0900

    [이거성 2021.06.28] -- 이메일 인증기능 구현 완료.

--- src/main/webapp/js/user.js
+++ src/main/webapp/js/user.js
@@ -300,11 +300,20 @@ function delFlagChk(){
 
 	//이메일 인증
 function emailChk(){
+	let emailWait = document.getElementById('emailWait');
+	//인증창이 뜨는동안 대기 메세지 활성화 및 버튼 비활성화
+	emailWait.innerHTML='인증창이 뜰때까지 기다려주세요.';
+	emailWait.style.color='blue';
+	let emailChkBtn = document.getElementById("emailChkBtn");
+	emailChkBtn.disabled = true;
 	$.ajax({
 		url:"./emailChk.do",
 		type:"post",
 		async:"true",
 		success: function(){
+			//인증창이뜨면 메세지 사라짐 및 버튼 활성화
+			emailWait.innerHTML="";
+			emailChkBtn.disabled = false;
 			Swal.fire({
 				title:'이메일 인증',
 				html:'이메일로 전송된 인증번호를 입력해 주세요.',
@@ -330,6 +339,8 @@ function emailChk(){
 								title:'이메일 인증',
 								text:'인증에 성공하셨습니다.'
 							})
+							// 인증에 성공하면 버튼 사라지도록
+							emailChkBtn.style.display = "none";
 							}else{
 								Swal.fire({
 									icon:'error',
